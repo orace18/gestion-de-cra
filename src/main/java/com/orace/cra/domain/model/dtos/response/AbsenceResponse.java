598e5ce9883538d1b7c3dce11c6d034f6a854791
Orace EDJO <oracejuscard@gmail.com>
2026-03-04 13:42:52 +0100

    Ajout d'abscence

--- src/main/java/com/orace/cra/domain/model/dtos/response/AbsenceResponse.java
+++ src/main/java/com/orace/cra/domain/model/dtos/response/AbsenceResponse.java
@@ -4,7 +4,6 @@ package com.orace.cra.domain.model.dtos.response;
 import com.orace.cra.domain.model.enums.DayType;
 import lombok.Builder;
 import lombok.Data;
-
 import java.time.LocalDate;
 
 @Data
--- src/main/java/com/orace/cra/domain/model/dtos/response/AbsenceSummaryResponse.java
+++ src/main/java/com/orace/cra/domain/model/dtos/response/AbsenceSummaryResponse.java
@@ -1,10 +1,7 @@
 package com.orace.cra.domain.model.dtos.response;
 
-
-
 import lombok.Builder;
 import lombok.Data;
-
 import java.util.List;
 import java.util.Map;
 
